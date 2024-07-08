3b13e2fcfe981fa39100bddeee0e074e5c2cc66c
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2024-07-08 14:21:08 +0200

    chore: Bump CLI (#4)

--- step.sh
+++ step.sh
@@ -28,7 +28,7 @@ echo "Bitrise Build Cache is activated in this workspace"
 set -x
 
 # download the Bitrise Build Cache CLI
-export BITRISE_BUILD_CACHE_CLI_VERSION=${BITRISE_BUILD_CACHE_CLI_VERSION:="v0.15.0-alpha.15+xcode"}
+export BITRISE_BUILD_CACHE_CLI_VERSION=${BITRISE_BUILD_CACHE_CLI_VERSION:="v0.15.0-alpha.16+xcode"}
 curl --retry 5 -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
 
 # the cli command without the -d flag
